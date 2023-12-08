a1d5994c94c8f5729f60068209724f658838ad97
Niclas Älmeby <37627447+zamomfg@users.noreply.github.com>
2023-12-08 14:39:12 +0000

    fixed users in user objects

--- GetVIP.ps1
+++ GetVIP.ps1
@@ -1,15 +1,57 @@
 
 $Array = @()
+$Users = @{}
 
-$users = Get-MgUser -All
+$AdUsers = Get-MgUser -All
 
-foreach ($user in $users) {
-    $Row = "" | Select UserUPN, ManagerUPN
+foreach ($user in $AdUsers) {
+    # $Row = "" | Select UserUPN, ManagerUPN
 
     $manager = Get-MgUserManager -UserID $user.UserPrincipalName -ErrorAction SilentlyContinue | %{Get-MgUser -UserId $_.Id}
-    $Row.UserUPN = $user.UserPrincipalName
-    $Row.ManagerUPN = $manager.UserPrincipalName
-    $Array += $Row
+    # $Row.UserUPN = $user.UserPrincipalName
+    # $Row.ManagerUPN = $manager.UserPrincipalName
+    # $Array += $Row
+
+    # Write-Output $user.UserPrincipalName
+
+    if($manager.UserPrincipalName -ne $null && $Users["$manager.UserPrincipalName"] -eq $null){
+        $ManagerObj = [User]::new()
+        $ManagerObj.ObjectId = $manager.Id
+        $ManagerObj.UPN = $manager.UserPrincipalName
+        $ManagerObj.Employees = @()
+
+        $Users[$ManagerObj.UPN] = $ManagerObj
+    }
+
+    if($Users[$user.UserPrincipalName] -eq $null){
+        $UserObj = [User]::new()
+        $UserObj.ObjectId = $user.Id
+        $UserObj.UPN = $user.UserPrincipalName
+        $UserObj.Manager = $ManagerObj
+        $UserObj.Employees = @()
+
+        # Write-Output $UserObj
+        # Write-Output $manager.UserPrincipalName
+        $Users[$user.UserPrincipalName] = $UserObj
+
+    }
+    
+    if($manager.UserPrincipalName -ne $null){
+        $Users[$manager.UserPrincipalName].Employees += $UserObj
+    }
 }
 
-Write-Output $Array
+
+foreach ($user in $Users.Values) {
+    Write-Output $user
+}
+
+# Write-Output $Users["LeeG@t20dk.onmicrosoft.com"].Employees
+# Write-Output $Users["LidiaH@t20dk.onmicrosoft.co"]
+
+class User {
+    [string]$ObjectId
+    [string]$UPN
+    [User[]]$Employees
+    [User]$Manager
+}
